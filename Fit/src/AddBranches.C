d59831ce474c38ab8c6e3f9c032a7f1cbd755c4c
pandolf <>
2012-11-20 17:57:51 +0000

    added nneutral_ptCut

--- Fit/src/AddBranches.C
+++ Fit/src/AddBranches.C
@@ -14,13 +14,15 @@ TTree *t=(TTree*)f->Get(A.ReadParameterFromFile("data/config.ini","TREENAME"));
 int nPFCandJet0,nPFCand_QCJet0; 
 TBranch *b1=t->Branch("nPFCandJet0",&nPFCandJet0,"nPFCandJet0/I");
 TBranch *b2=t->Branch("nPFCand_QCJet0",&nPFCand_QCJet0,"nPFCand_QCJet0/I");
+TBranch *b3=t->Branch("nPFCand_QC_ptCutJet0",&nPFCand_QC_ptCutJet0,"nPFCand_QC_ptCutJet0/I");
 
-int nChg,nCharged,nNeutral;
+int nChg,nCharged,nNeutral,nNeutral_ptCut;
 //SetBranchAddress
 if(what==0){
 t->SetBranchAddress("nChg_QCJet0",&nChg);
 t->SetBranchAddress("nChargedJet0",&nCharged);
 t->SetBranchAddress("nNeutralJet0",&nNeutral);
+t->SetBranchAddress("nNeutral_ptCutJet0",&nNeutral_ptCut);
 }
 
 long long nEntries=t->GetEntries();
@@ -30,8 +32,10 @@ for(long long int i=0;i<nEntries;++i){
 	if(what==0){
 		nPFCandJet0=nCharged+nNeutral;
 		nPFCand_QCJet0=nChg+nNeutral;
+		nPFCand_QC_ptCutJet0=nChg+nNeutral_ptCut;
 		b1->Fill();
 		b2->Fill();
+		b3->Fill();
 		}
 	}
 //Write the Tree (With OverWrite Option)
