2d735121de655418bcdbcf9c62f4843b46e6a453
pandolf <>
2013-02-20 15:48:03 +0000

    first commit

---
+++ Ntp1Analyzer_TTbarWjj.h
@@ -0,0 +1,172 @@
+//------------------------------------------------------------------
+//
+//    Derived Ntp1Analyzer class. Inherits from Ntp1Analyzer.
+//    Reads output of e/c/p tree dumper, and produces subtrees,
+//    to be used in the H->ZZ->lljj analysis.
+//
+//------------------------------------------------------------------
+
+
+#ifndef Ntp1Analyzer_TTbarWjj_h
+#define Ntp1Analyzer_TTbarWjj_h
+
+#include "Ntp1Analyzer.h"
+#include "TH1F.h"
+
+#include "UserCode/pandolf/QGLikelihood/interface/QGLikelihoodCalculator.h"
+
+
+class Ntp1Analyzer_TTbarWjj : public Ntp1Analyzer {
+
+ public:
+
+   Ntp1Analyzer_TTbarWjj( const std::string& dataset, const std::string& flags="", TTree* tree=0);
+   virtual ~Ntp1Analyzer_TTbarWjj();
+
+   virtual void CreateOutputFile();
+   virtual void Loop();
+
+
+
+
+ private:
+
+   QGLikelihoodCalculator* qglc_;
+
+   Bool_t passed_HLT_DoubleMu6_;
+   Bool_t passed_HLT_DoubleMu7_;
+   Bool_t passed_HLT_Mu13_Mu8_;
+   Bool_t passed_HLT_Mu17_Mu8_;
+   Bool_t passed_HLT_IsoMu17_;
+   Bool_t passed_HLT_IsoMu24_;
+   Bool_t passed_HLT_IsoMu24_eta2p1_;
+   Bool_t passed_HLT_Mu8_Jet40_;
+   Bool_t passed_HLT_L2DoubleMu23_NoVertex_;
+   Bool_t passed_HLT_L2DoubleMu30_NoVertex_;
+   Bool_t passed_HLT_TripleMu5_;
+
+   Bool_t passed_HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_;
+   Bool_t passed_HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_;
+   Bool_t passed_HLT_Ele17_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_Ele8_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_;
+
+   Bool_t passed_HLT_Mu17_Ele8_CaloIdL_;
+   Bool_t passed_HLT_Mu8_Ele17_CaloIdL_;
+   Bool_t passed_HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_;
+
+   int leptType_; //0: muon; 1: electron; 2: e+m-; 3: e-m+
+   int leptTypeMC_; //0: muon; 1: electron
+
+
+   Float_t eLept1_;
+   Float_t ptLept1_;
+   Float_t etaLept1_;
+   Float_t phiLept1_;
+   Int_t   chargeLept1_;
+   Float_t combinedIsoRelLept1_;
+   bool matchedToHLTLept1_;
+
+
+   int nLept_;
+   Int_t   leptTypeLept_[10];
+   Float_t eLept_[10];
+   Float_t ptLept_[10];
+   Float_t etaLept_[10];
+   Float_t phiLept_[10];
+   Int_t   chargeLept_[10];
+   Float_t combinedIsoRelLept_[10];
+
+   Float_t eLeptGen_[10];
+   Float_t ptLeptGen_[10];
+   Float_t etaLeptGen_[10];
+   Float_t phiLeptGen_[10];
+
+
+   Int_t nJets_;
+
+   Float_t  ptJet_[50];
+   Float_t   eJet_[50];
+   Float_t phiJet_[50];
+   Float_t etaJet_[50];
+   Float_t  ptUncertJet_[50];
+
+   Float_t ptDJet_[50];
+   Float_t ptD_QCJet_[50];
+   Float_t axis1_QCJet_[50];
+   Float_t axis2_QCJet_[50];
+   Float_t rmsCandJet_[50];
+   Int_t nChargedJet_[50];
+   Int_t nNeutralJet_[50];
+   Int_t nPFCand_QC_ptCutJet_[50];
+   Float_t qglJet_[50];
+
+   Float_t  eChargedHadronsJet_[50];
+   Float_t  ePhotonsJet_[50];
+   Float_t  eNeutralEmJet_[50];
+   Float_t  eNeutralHadronsJet_[50];
+   Float_t  eMuonsJet_[50];
+   Float_t  eElectronsJet_[50];
+   Float_t  eHFHadronsJet_[50];
+   Float_t  eHFEMJet_[50];
+
+   Int_t  nChargedHadronsJet_[50];
+   Int_t  nPhotonsJet_[50];
+   Int_t  nNeutralHadronsJet_[50];
+   Int_t  nMuonsJet_[50];
+   Int_t  nElectronsJet_[50];
+   Int_t  nHFHadronsJet_[50];
+   Int_t  nHFEMJet_[50];
+
+   Float_t trackCountingHighEffBJetTagJet_[50];
+   Float_t trackCountingHighPurBJetTagJet_[50];
+   Float_t simpleSecondaryVertexHighEffBJetTagJet_[50];
+   Float_t simpleSecondaryVertexHighPurBJetTagJet_[50];
+   Float_t jetBProbabilityBJetTagJet_[50];
+   Float_t jetProbabilityBJetTagJet_[50];
+   Float_t combinedSecondaryVertexBJetTagJet_[50];
+
+   Float_t betaStarJet_[50];
+
+   Float_t SFTCHEJet_[50];
+   Float_t SFerrTCHEJet_[50];
+
+   Float_t  ptGenJet_[50];
+   Float_t   eGenJet_[50];
+   Float_t phiGenJet_[50];
+   Float_t etaGenJet_[50];
+
+   Float_t  ptPartJet_[50];
+   Float_t   ePartJet_[50];
+   Float_t phiPartJet_[50];
+   Float_t etaPartJet_[50];
+   Int_t pdgIdPartJet_[50];
+
+
+
+   Int_t nPart_;
+   Float_t  ptPart_[20];
+   Float_t   ePart_[20];
+   Float_t phiPart_[20];
+   Float_t etaPart_[20];
+   Int_t pdgIdPart_[20];
+   Int_t motherPart_[20];
+
+
+   Float_t epfMet_;
+   Float_t sumEtpfMet_;
+   Float_t metSignificance_;
+   Float_t mEtSig_;
+   Float_t phipfMet_;
+
+   TH1D* h1_nPU_gen_;
+
+   TH1D* h1_nCounter_Zee_;
+   TH1D* h1_nCounter_Zmumu_;
+
+   bool DEBUG_VERBOSE_;
+
+};
+
+
+
+
+#endif
---
+++ do2ndLevel_TTbarWjj.cpp
@@ -0,0 +1,82 @@
+#include "Ntp1Analyzer_TTbarWjj.h"
+#include <stdlib.h>
+
+#include "TString.h"
+
+
+
+int main( int argc, char* argv[]) {
+
+  if( argc!=2 && argc!=3 && argc!=4 ) {
+    std::cout << "USAGE: ./do2ndLevel_TTbarWjj [dataset] [inputfile=\"\"] [flags=\"\"]" << std::endl;
+    exit(31);
+  }
+
+  std::string dataset(argv[1]);
+
+  Ntp1Analyzer_TTbarWjj* na;
+
+  if( argc<4 ) {
+    na = new Ntp1Analyzer_TTbarWjj(dataset);
+  } else {
+    std::string flags(argv[3]);
+    na = new Ntp1Analyzer_TTbarWjj(dataset, flags);
+  }
+
+  TString dataset_tstr(dataset);
+
+  bool isData2011 = dataset_tstr.Contains("Run2011");
+  bool isData2012 = dataset_tstr.Contains("Run2012");
+
+  if( isData2011 || isData2012 ) {
+
+    if( dataset_tstr.BeginsWith("DoubleMu") ) {
+
+      na->AddRequiredTrigger( "HLT_DoubleMu7_v" );
+      na->AddRequiredTrigger( "HLT_Mu13_Mu8_v" );
+      na->AddRequiredTrigger( "HLT_Mu17_Mu8_v" );
+
+    } else if( dataset_tstr.BeginsWith("DoubleElectron") ) {
+
+      na->AddRequiredTrigger( "HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v" );
+      na->AddRequiredTrigger( "HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v" );
+      na->AddRequiredTrigger( "HLT_Ele17_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_Ele8_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_v" );
+ 
+    } else if( dataset_tstr.BeginsWith("MuEG") ) {
+
+      na->AddRequiredTrigger( "HLT_Mu17_Ele8_CaloIdL_v" );
+      na->AddRequiredTrigger( "HLT_Mu8_Ele17_CaloIdL_v" );
+      na->AddRequiredTrigger( "HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_v" );
+
+    } else if( dataset_tstr.BeginsWith("SingleMu") ) {
+
+      na->AddRequiredTrigger( "HLT_IsoMu24_v" );
+      na->AddRequiredTrigger( "HLT_IsoMu24_eta2p1_v" );
+
+    }
+
+    //if( isData2011 )
+    //  na->ReadJSONFile("Cert_160404-180252_7TeV_PromptReco_Collisions11_CMSSWConfig.txt");
+    //if( isData2012 )
+    //  na->ReadJSONFile("Cert_190456-200245_8TeV_PromptReco_Collisions12_CMSSWConfig.txt");
+
+  }  //if is data
+
+
+
+  if( argc==2 ) {
+    std::string fileName = "files_" + dataset + ".txt";
+    na->LoadInputFromFile(fileName.c_str());
+    //na->LoadInput();
+  } else {
+    std::string inputfile(argv[2]);
+    na->LoadInputFromFile(inputfile.c_str());
+  }
+
+  na->Loop();
+
+  delete na;
+
+  return 0;
+
+}
